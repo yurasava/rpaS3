89ebd65f3392bea32cb39c8ec6a549dc681123b0
Yury Savitski <78652692+yurasava@users.noreply.github.com>
2021-02-17 01:27:37 +0300

    Update Application.java

--- aTemplate/src/main/java/by/jrr/start/Application.java
+++ aTemplate/src/main/java/by/jrr/start/Application.java
@@ -3,6 +3,6 @@ package by.jrr.start;
 public class Application {
 
     public static void main(String[] args) {
-        logger.log("Hello World");
+        System.out.println("Hello World");
     }
 }
