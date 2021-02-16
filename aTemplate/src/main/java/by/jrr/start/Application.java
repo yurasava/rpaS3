4d0ff6934cf2f6d29cec1a8a99697543f42c9fba
Yury Savitski <78652692+yurasava@users.noreply.github.com>
2021-02-17 00:41:15 +0300

    replacement logger.log

--- aTemplate/src/main/java/by/jrr/start/Application.java
+++ aTemplate/src/main/java/by/jrr/start/Application.java
@@ -3,6 +3,6 @@ package by.jrr.start;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        logger.log("Hello World");
     }
 }
